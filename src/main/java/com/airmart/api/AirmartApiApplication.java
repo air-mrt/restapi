8509e0aeae5fbb013400506e53d9ea06305c1e6d
eyuel <eyuel.berga@yahoo.com>
2019-06-07 14:19:25 +0300

    initial commit

---
+++ src/main/java/com/airmart/api/AirmartApiApplication.java
@@ -0,0 +1,13 @@
+package com.airmart.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirmartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirmartApiApplication.class, args);
+	}
+
+}
